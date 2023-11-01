75770a7741dec818005707cd0215ee243e8bddb8
ds5387391@gmail.com <ds5387391@gmail.com>
2023-11-01 06:40:16 +0000

    Saved recipe: DEMO1.compute_data_prepared

---
+++ recipes/compute_data_prepared.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import dataiku
+import pandas as pd, numpy as np
+from dataiku import pandasutils as pdu
+
+# Read recipe inputs
+data1 = dataiku.Dataset("data1")
+data1_df = data1.get_dataframe()
+
+
+# Compute recipe outputs from inputs
+# TODO: Replace this part by your actual code that computes the output, as a Pandas dataframe
+# NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
+
+data_prepared_df = data1_df # For this sample code, simply copy input to output
+
+
+# Write recipe outputs
+data_prepared = dataiku.Dataset("data_prepared")
+data_prepared.write_with_schema(data_prepared_df)
